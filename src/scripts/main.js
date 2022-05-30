a940ad80bf64acfa6399377ff97070c2258dbde4
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-05-30 14:16:34 +0200

    added js, btn and animations
    added...
    menu icon
    menu btn
    btn functionality
    btn animation

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+let MenuBtn = document.querySelector('#menu-btn');
+let sideNav = document.querySelector('#side-nav');
+
+
+function appear(){
+    sideNav.style.display= "block";
+}
+
+function disappear(){
+    sideNav.style.display="none"
+}
